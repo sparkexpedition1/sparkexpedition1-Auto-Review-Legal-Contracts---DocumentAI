2aa79d40274f7e2096b89f9d22127e12b82078ef
sparkexpedition1 <61966148+sparkexpedition1@users.noreply.github.com>
2022-10-14 10:36:59 +0000

    Update main.py

--- main.py
+++ main.py
@@ -63,6 +63,7 @@ def st_ui():
    
   if fileupload:
     text=[]
+    st.text(fileupload')
     pdfbytes = fileupload.getvalue()
     if select_category == "PDF":
       doc = return_doc_from_bytes(pdfbytes)
