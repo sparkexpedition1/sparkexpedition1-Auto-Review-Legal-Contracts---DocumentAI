ebf78d3058a7861d727cc7c5b02526b6ccefac7f
sparkexpedition1 <61966148+sparkexpedition1@users.noreply.github.com>
2022-10-18 14:35:20 +0000

    Update main.py

--- main.py
+++ main.py
@@ -68,13 +68,13 @@ def st_ui():
       for page in doc:
         text+=(page.get_text().split('\n'))
         #st.text('debug point 1')
-      st.text(text)
+      #st.text(text)
     if select_category =="Word Document":
       doc = docx.Document(fileupload)
       for i in range(len(doc.paragraphs)):
         text+=(doc.paragraphs[i].text)
     cleaned_document=preprocessing(text)
-    #st.text('debug point 2')
+    st.text('cleaned_document')
     if select_category == "PPT":
       pass
     if Enter_text:
