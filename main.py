e783e61ff4eae15d5e06c609c1016b5c8c94e183
sparkexpedition1 <61966148+sparkexpedition1@users.noreply.github.com>
2022-10-19 03:00:53 +0000

    Update main.py

--- main.py
+++ main.py
@@ -146,8 +146,10 @@ def st_ui():
         result=search_report(cleaned_document,search_text.lower())
         st.header('Related information linked to Search Content')
         info=''
+        find_instance=1
         for i in result:
-            info+=i+" "
+            info+=str(find_instance) + ":" + i +"\n"
+            find_instance=find_instance + 1
         st.write(info)
     
     if select_category == "Simplify Content":
